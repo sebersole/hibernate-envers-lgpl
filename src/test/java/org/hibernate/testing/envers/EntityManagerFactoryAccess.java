cf9425c0a04bdf4ff071fe26de0b4f044a3a021d
Andrea Boriero <andrea@hibernate.org>
2021-04-09 11:10:36 +0200

    Remove o.h.testing.junit5 package

---
+++ src/test/java/org/hibernate/testing/envers/EntityManagerFactoryAccess.java
@@ -0,0 +1,27 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or http://www.gnu.org/licenses/lgpl-2.1.html
+ */
+package org.hibernate.testing.envers;
+
+import javax.persistence.EntityManagerFactory;
+
+import org.hibernate.dialect.Dialect;
+
+import org.hibernate.testing.orm.junit.DialectContext;
+
+/**
+ * Contract for things that expose an EntityManagerFactory
+ *
+ * @author Chris Cranford
+ */
+public interface EntityManagerFactoryAccess extends DialectAccess {
+	EntityManagerFactory getEntityManagerFactory();
+
+	@Override
+	default Dialect getDialect() {
+		return DialectContext.getDialect();
+	}
+}
