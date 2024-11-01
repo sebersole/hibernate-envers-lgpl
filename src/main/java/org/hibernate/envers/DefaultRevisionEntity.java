mbladel/hibernate-orm

HHH-17612 HHH-18762 Separate envers default revision entity types from mapped-superclasses
Also, ensure they're query-able through Criteria and HQL queries

hibernate-envers/src/main/java/org/hibernate/envers/DefaultRevisionEntity.java
@@ -4,76 +4,12 @@
  */
 package org.hibernate.envers;
 
-import java.io.Serializable;
-import java.util.Date;
-
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
-import jakarta.persistence.Transient;
+import jakarta.persistence.Entity;
 
 /**
  * @author Adam Warski (adam at warski dot org)
  * @author Chris Cranford
  */
-@MappedSuperclass
-public class DefaultRevisionEntity implements Serializable {
-	private static final long serialVersionUID = 8530213963961662300L;
-
-	@Id
-	@GeneratedValue
-	@RevisionNumber
-	private int id;
-
-	@RevisionTimestamp
-	private long timestamp;
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	@Transient
-	public Date getRevisionDate() {
-		return new Date( timestamp );
-	}
-
-	public long getTimestamp() {
-		return timestamp;
-	}
-
-	public void setTimestamp(long timestamp) {
-		this.timestamp = timestamp;
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		if ( this == o ) {
-			return true;
-		}
-		if ( !(o instanceof DefaultRevisionEntity) ) {
-			return false;
-		}
-
-		final DefaultRevisionEntity that = (DefaultRevisionEntity) o;
-		return id == that.id
-				&& timestamp == that.timestamp;
-	}
-
-	@Override
-	public int hashCode() {
-		int result;
-		result = id;
-		result = 31 * result + (int) (timestamp ^ (timestamp >>> 32));
-		return result;
-	}
-
-	@Override
-	public String toString() {
-		return "DefaultRevisionEntity(id = " + id
-				+ ", revisionDate = " + DateTimeFormatter.INSTANCE.format(getRevisionDate() ) + ")";
-	}
+@Entity
+public final class DefaultRevisionEntity extends DefaultRevisionType {
 }

hibernate-envers/src/main/java/org/hibernate/envers/DefaultTrackingModifiedEntitiesRevisionEntity.java
@@ -4,17 +4,7 @@
  */
 package org.hibernate.envers;
 
-import java.util.HashSet;
-import java.util.Set;
-import jakarta.persistence.Column;
-import jakarta.persistence.ElementCollection;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.MappedSuperclass;
-
-import org.hibernate.annotations.Fetch;
-import org.hibernate.annotations.FetchMode;
+import jakarta.persistence.Entity;
 
 /**
  * Extension of standard {@link DefaultRevisionEntity} that allows tracking entity names changed in each revision.
@@ -23,54 +13,6 @@ import org.hibernate.annotations.FetchMode;
  *
  * @author Lukasz Antoniak (lukasz dot antoniak at gmail dot com)
  */
-@MappedSuperclass
-public class DefaultTrackingModifiedEntitiesRevisionEntity extends DefaultRevisionEntity {
-	@ElementCollection(fetch = FetchType.EAGER)
-	@JoinTable(name = "REVCHANGES", joinColumns = @JoinColumn(name = "REV"))
-	@Column(name = "ENTITYNAME")
-	@Fetch(FetchMode.JOIN)
-	@ModifiedEntityNames
-	private Set<String> modifiedEntityNames = new HashSet<>();
-
-	public Set<String> getModifiedEntityNames() {
-		return modifiedEntityNames;
-	}
-
-	public void setModifiedEntityNames(Set<String> modifiedEntityNames) {
-		this.modifiedEntityNames = modifiedEntityNames;
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		if ( this == o ) {
-			return true;
-		}
-		if ( !(o instanceof DefaultTrackingModifiedEntitiesRevisionEntity) ) {
-			return false;
-		}
-		if ( !super.equals( o ) ) {
-			return false;
-		}
-
-		final DefaultTrackingModifiedEntitiesRevisionEntity that = (DefaultTrackingModifiedEntitiesRevisionEntity) o;
-
-		if ( modifiedEntityNames != null ? !modifiedEntityNames.equals( that.modifiedEntityNames )
-				: that.modifiedEntityNames != null ) {
-			return false;
-		}
-
-		return true;
-	}
-
-	@Override
-	public int hashCode() {
-		int result = super.hashCode();
-		result = 31 * result + (modifiedEntityNames != null ? modifiedEntityNames.hashCode() : 0);
-		return result;
-	}
-
-	@Override
-	public String toString() {
-		return "DefaultTrackingModifiedEntitiesRevisionEntity(" + super.toString() + ", modifiedEntityNames = " + modifiedEntityNames + ")";
-	}
+@Entity
+public final class DefaultTrackingModifiedEntitiesRevisionEntity extends DefaultTrackingModifiedEntitiesRevisionType {
 }

hibernate-envers/src/main/java/org/hibernate/envers/enhanced/SequenceIdRevisionEntity.java
@@ -4,92 +4,13 @@
  */
 package org.hibernate.envers.enhanced;
 
-import java.io.Serializable;
-import java.util.Date;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
-import jakarta.persistence.Transient;
-
-import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.Parameter;
-import org.hibernate.envers.DateTimeFormatter;
-import org.hibernate.envers.RevisionNumber;
-import org.hibernate.envers.RevisionTimestamp;
+import jakarta.persistence.Entity;
 
 /**
  * @author Adam Warski (adam at warski dot org)
  * @author Lukasz Antoniak (lukasz dot antoniak at gmail dot com)
  * @author Chris Cranford
  */
-@MappedSuperclass
-public class SequenceIdRevisionEntity implements Serializable {
-	private static final long serialVersionUID = 4159156677698841902L;
-
-	@Id
-	@GeneratedValue(generator = "RevisionNumberSequenceGenerator")
-	@GenericGenerator(
-			name = "RevisionNumberSequenceGenerator",
-			strategy = "org.hibernate.envers.enhanced.OrderedSequenceGenerator",
-			parameters = {
-					@Parameter(name = "table_name", value = "REVISION_GENERATOR"),
-					@Parameter(name = "sequence_name", value = "REVISION_GENERATOR"),
-					@Parameter(name = "initial_value", value = "1"),
-					@Parameter(name = "increment_size", value = "1")
-			}
-	)
-	@RevisionNumber
-	private int id;
-
-	@RevisionTimestamp
-	private long timestamp;
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	@Transient
-	public Date getRevisionDate() {
-		return new Date( timestamp );
-	}
-
-	public long getTimestamp() {
-		return timestamp;
-	}
-
-	@SuppressWarnings("unused")
-	public void setTimestamp(long timestamp) {
-		this.timestamp = timestamp;
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		if ( this == o ) {
-			return true;
-		}
-		if ( !(o instanceof SequenceIdRevisionEntity) ) {
-			return false;
-		}
-
-		final SequenceIdRevisionEntity that = (SequenceIdRevisionEntity) o;
-		return id == that.id && timestamp == that.timestamp;
-	}
-
-	@Override
-	public int hashCode() {
-		int result = id;
-		result = 31 * result + (int) (timestamp ^ (timestamp >>> 32));
-		return result;
-	}
-
-	@Override
-	public String toString() {
-		return "SequenceIdRevisionEntity(id = " + id
-				+ ", revisionDate = " + DateTimeFormatter.INSTANCE.format(getRevisionDate() )
-				+ ")";
-	}
+@Entity
+public final class SequenceIdRevisionEntity extends SequenceIdRevisionType {
 }

hibernate-envers/src/main/java/org/hibernate/envers/enhanced/SequenceIdTrackingModifiedEntitiesRevisionEntity.java
@@ -4,18 +4,7 @@
  */
 package org.hibernate.envers.enhanced;
 
-import java.util.HashSet;
-import java.util.Set;
-import jakarta.persistence.Column;
-import jakarta.persistence.ElementCollection;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.MappedSuperclass;
-
-import org.hibernate.annotations.Fetch;
-import org.hibernate.annotations.FetchMode;
-import org.hibernate.envers.ModifiedEntityNames;
+import jakarta.persistence.Entity;
 
 /**
  * Extension of standard {@link SequenceIdRevisionEntity} that allows tracking entity names changed in each revision.
@@ -24,57 +13,6 @@ import org.hibernate.envers.ModifiedEntityNames;
  *
  * @author Lukasz Antoniak (lukasz dot antoniak at gmail dot com)
  */
-@MappedSuperclass
-public class SequenceIdTrackingModifiedEntitiesRevisionEntity extends SequenceIdRevisionEntity {
-	@ElementCollection(fetch = FetchType.EAGER)
-	@JoinTable(name = "REVCHANGES", joinColumns = @JoinColumn(name = "REV"))
-	@Column(name = "ENTITYNAME")
-	@Fetch(FetchMode.JOIN)
-	@ModifiedEntityNames
-	private Set<String> modifiedEntityNames = new HashSet<>();
-
-	@SuppressWarnings("unused")
-	public Set<String> getModifiedEntityNames() {
-		return modifiedEntityNames;
-	}
-
-	@SuppressWarnings("unused")
-	public void setModifiedEntityNames(Set<String> modifiedEntityNames) {
-		this.modifiedEntityNames = modifiedEntityNames;
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		if ( this == o ) {
-			return true;
-		}
-		if ( !(o instanceof SequenceIdTrackingModifiedEntitiesRevisionEntity) ) {
-			return false;
-		}
-		if ( !super.equals( o ) ) {
-			return false;
-		}
-
-		final SequenceIdTrackingModifiedEntitiesRevisionEntity that = (SequenceIdTrackingModifiedEntitiesRevisionEntity) o;
-
-		if ( modifiedEntityNames == null ) {
-			return that.modifiedEntityNames == null;
-		}
-		else {
-			return modifiedEntityNames.equals( that.modifiedEntityNames );
-		}
-	}
-
-	@Override
-	public int hashCode() {
-		int result = super.hashCode();
-		result = 31 * result + (modifiedEntityNames != null ? modifiedEntityNames.hashCode() : 0);
-		return result;
-	}
-
-	@Override
-	public String toString() {
-		return "SequenceIdTrackingModifiedEntitiesRevisionEntity(" + super.toString()
-				+ ", modifiedEntityNames = " + modifiedEntityNames + ")";
-	}
+@Entity
+public final class SequenceIdTrackingModifiedEntitiesRevisionEntity extends SequenceIdTrackingModifiedEntitiesRevisionType {
 }

hibernate-envers/src/test/java/org/hibernate/orm/test/envers/CustomRevisionEntityTest.java
@@ -8,7 +8,7 @@ import java.util.Date;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.envers.Audited;
-import org.hibernate.envers.DefaultRevisionEntity;
+import org.hibernate.envers.DefaultRevisionType;
 import org.hibernate.envers.RevisionEntity;
 import org.hibernate.envers.RevisionListener;
 
@@ -124,7 +124,7 @@ public class CustomRevisionEntityTest {
 	@Entity(name = "CustomRevisionEntity")
 	@Table(name = "CUSTOM_REV_INFO")
 	@RevisionEntity(CustomRevisionEntityListener.class)
-	public static class CustomRevisionEntity extends DefaultRevisionEntity {
+	public static class CustomRevisionEntity extends DefaultRevisionType {
 
 		private String username;
 

hibernate-envers/src/test/java/org/hibernate/orm/test/envers/EntityTypeChangeAuditDefaultTrackingTest.java
@@ -19,7 +19,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.engine.spi.SessionFactoryImplementor;
 import org.hibernate.envers.Audited;
-import org.hibernate.envers.DefaultTrackingModifiedEntitiesRevisionEntity;
+import org.hibernate.envers.DefaultTrackingModifiedEntitiesRevisionType;
 import org.hibernate.envers.RevisionEntity;
 import org.hibernate.jpa.boot.spi.Bootstrap;
 
@@ -198,7 +198,7 @@ public class EntityTypeChangeAuditDefaultTrackingTest extends EntityManagerFacto
 	@Table(name = "TRACKING_REV_INFO")
 	@RevisionEntity
 	public static class CustomTrackingRevisionEntity
-		extends DefaultTrackingModifiedEntitiesRevisionEntity {
+		extends DefaultTrackingModifiedEntitiesRevisionType {
 
 	}
 	//end::envers-tracking-modified-entities-revchanges-example[]

hibernate-envers/src/test/java/org/hibernate/orm/test/envers/EntityTypeChangeAuditTest.java
@@ -25,7 +25,7 @@ import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.engine.spi.SessionFactoryImplementor;
 import org.hibernate.envers.AuditReaderFactory;
 import org.hibernate.envers.Audited;
-import org.hibernate.envers.DefaultRevisionEntity;
+import org.hibernate.envers.DefaultRevisionType;
 import org.hibernate.envers.ModifiedEntityNames;
 import org.hibernate.envers.RevisionEntity;
 import org.hibernate.jpa.boot.spi.Bootstrap;
@@ -238,7 +238,7 @@ public class EntityTypeChangeAuditTest extends EntityManagerFactoryBasedFunction
 	@Entity(name = "CustomTrackingRevisionEntity")
 	@Table(name = "TRACKING_REV_INFO")
 	@RevisionEntity
-	public static class CustomTrackingRevisionEntity extends DefaultRevisionEntity {
+	public static class CustomTrackingRevisionEntity extends DefaultRevisionType {
 
 		@ElementCollection
 		@JoinTable(

hibernate-envers/src/test/java/org/hibernate/orm/test/envers/integration/reventity/InheritedRevEntity.java
@@ -7,12 +7,12 @@ package org.hibernate.orm.test.envers.integration.reventity;
 import jakarta.persistence.Entity;
 
 import org.hibernate.envers.RevisionEntity;
-import org.hibernate.envers.enhanced.SequenceIdRevisionEntity;
+import org.hibernate.envers.enhanced.SequenceIdRevisionType;
 
 /**
  * @author Adam Warski (adam at warski dot org)
  */
 @Entity
 @RevisionEntity
-public class InheritedRevEntity extends SequenceIdRevisionEntity {
+public class InheritedRevEntity extends SequenceIdRevisionType {
 }

sebersole/hibernate-envers-lgpl

src/main/java/org/hibernate/envers/DefaultRevisionEntity.java
@@ -4,76 +4,12 @@
  */
 package org.hibernate.envers;
 
-import java.io.Serializable;
-import java.util.Date;
-
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
-import jakarta.persistence.Transient;
+import jakarta.persistence.Entity;
 
 /**
  * @author Adam Warski (adam at warski dot org)
  * @author Chris Cranford
  */
-@MappedSuperclass
-public class DefaultRevisionEntity implements Serializable {
-	private static final long serialVersionUID = 8530213963961662300L;
-
-	@Id
-	@GeneratedValue
-	@RevisionNumber
-	private int id;
-
-	@RevisionTimestamp
-	private long timestamp;
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	@Transient
-	public Date getRevisionDate() {
-		return new Date( timestamp );
-	}
-
-	public long getTimestamp() {
-		return timestamp;
-	}
-
-	public void setTimestamp(long timestamp) {
-		this.timestamp = timestamp;
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		if ( this == o ) {
-			return true;
-		}
-		if ( !(o instanceof DefaultRevisionEntity) ) {
-			return false;
-		}
-
-		final DefaultRevisionEntity that = (DefaultRevisionEntity) o;
-		return id == that.id
-				&& timestamp == that.timestamp;
-	}
-
-	@Override
-	public int hashCode() {
-		int result;
-		result = id;
-		result = 31 * result + (int) (timestamp ^ (timestamp >>> 32));
-		return result;
-	}
-
-	@Override
-	public String toString() {
-		return "DefaultRevisionEntity(id = " + id
-				+ ", revisionDate = " + DateTimeFormatter.INSTANCE.format(getRevisionDate() ) + ")";
-	}
+@Entity
+public final class DefaultRevisionEntity extends RevisionMapping {
 }

src/main/java/org/hibernate/envers/DefaultTrackingModifiedEntitiesRevisionEntity.java
@@ -4,17 +4,7 @@
  */
 package org.hibernate.envers;
 
-import java.util.HashSet;
-import java.util.Set;
-import jakarta.persistence.Column;
-import jakarta.persistence.ElementCollection;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.MappedSuperclass;
-
-import org.hibernate.annotations.Fetch;
-import org.hibernate.annotations.FetchMode;
+import jakarta.persistence.Entity;
 
 /**
  * Extension of standard {@link DefaultRevisionEntity} that allows tracking entity names changed in each revision.
@@ -23,54 +13,6 @@ import org.hibernate.annotations.FetchMode;
  *
  * @author Lukasz Antoniak (lukasz dot antoniak at gmail dot com)
  */
-@MappedSuperclass
-public class DefaultTrackingModifiedEntitiesRevisionEntity extends DefaultRevisionEntity {
-	@ElementCollection(fetch = FetchType.EAGER)
-	@JoinTable(name = "REVCHANGES", joinColumns = @JoinColumn(name = "REV"))
-	@Column(name = "ENTITYNAME")
-	@Fetch(FetchMode.JOIN)
-	@ModifiedEntityNames
-	private Set<String> modifiedEntityNames = new HashSet<>();
-
-	public Set<String> getModifiedEntityNames() {
-		return modifiedEntityNames;
-	}
-
-	public void setModifiedEntityNames(Set<String> modifiedEntityNames) {
-		this.modifiedEntityNames = modifiedEntityNames;
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		if ( this == o ) {
-			return true;
-		}
-		if ( !(o instanceof DefaultTrackingModifiedEntitiesRevisionEntity) ) {
-			return false;
-		}
-		if ( !super.equals( o ) ) {
-			return false;
-		}
-
-		final DefaultTrackingModifiedEntitiesRevisionEntity that = (DefaultTrackingModifiedEntitiesRevisionEntity) o;
-
-		if ( modifiedEntityNames != null ? !modifiedEntityNames.equals( that.modifiedEntityNames )
-				: that.modifiedEntityNames != null ) {
-			return false;
-		}
-
-		return true;
-	}
-
-	@Override
-	public int hashCode() {
-		int result = super.hashCode();
-		result = 31 * result + (modifiedEntityNames != null ? modifiedEntityNames.hashCode() : 0);
-		return result;
-	}
-
-	@Override
-	public String toString() {
-		return "DefaultTrackingModifiedEntitiesRevisionEntity(" + super.toString() + ", modifiedEntityNames = " + modifiedEntityNames + ")";
-	}
+@Entity
+public final class DefaultTrackingModifiedEntitiesRevisionEntity extends TrackingModifiedEntitiesRevisionMapping {
 }

src/main/java/org/hibernate/envers/enhanced/SequenceIdRevisionEntity.java
@@ -4,92 +4,13 @@
  */
 package org.hibernate.envers.enhanced;
 
-import java.io.Serializable;
-import java.util.Date;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
-import jakarta.persistence.Transient;
-
-import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.Parameter;
-import org.hibernate.envers.DateTimeFormatter;
-import org.hibernate.envers.RevisionNumber;
-import org.hibernate.envers.RevisionTimestamp;
+import jakarta.persistence.Entity;
 
 /**
  * @author Adam Warski (adam at warski dot org)
  * @author Lukasz Antoniak (lukasz dot antoniak at gmail dot com)
  * @author Chris Cranford
  */
-@MappedSuperclass
-public class SequenceIdRevisionEntity implements Serializable {
-	private static final long serialVersionUID = 4159156677698841902L;
-
-	@Id
-	@GeneratedValue(generator = "RevisionNumberSequenceGenerator")
-	@GenericGenerator(
-			name = "RevisionNumberSequenceGenerator",
-			strategy = "org.hibernate.envers.enhanced.OrderedSequenceGenerator",
-			parameters = {
-					@Parameter(name = "table_name", value = "REVISION_GENERATOR"),
-					@Parameter(name = "sequence_name", value = "REVISION_GENERATOR"),
-					@Parameter(name = "initial_value", value = "1"),
-					@Parameter(name = "increment_size", value = "1")
-			}
-	)
-	@RevisionNumber
-	private int id;
-
-	@RevisionTimestamp
-	private long timestamp;
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	@Transient
-	public Date getRevisionDate() {
-		return new Date( timestamp );
-	}
-
-	public long getTimestamp() {
-		return timestamp;
-	}
-
-	@SuppressWarnings("unused")
-	public void setTimestamp(long timestamp) {
-		this.timestamp = timestamp;
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		if ( this == o ) {
-			return true;
-		}
-		if ( !(o instanceof SequenceIdRevisionEntity) ) {
-			return false;
-		}
-
-		final SequenceIdRevisionEntity that = (SequenceIdRevisionEntity) o;
-		return id == that.id && timestamp == that.timestamp;
-	}
-
-	@Override
-	public int hashCode() {
-		int result = id;
-		result = 31 * result + (int) (timestamp ^ (timestamp >>> 32));
-		return result;
-	}
-
-	@Override
-	public String toString() {
-		return "SequenceIdRevisionEntity(id = " + id
-				+ ", revisionDate = " + DateTimeFormatter.INSTANCE.format(getRevisionDate() )
-				+ ")";
-	}
+@Entity
+public final class SequenceIdRevisionEntity extends SequenceIdRevisionMapping {
 }

src/main/java/org/hibernate/envers/enhanced/SequenceIdTrackingModifiedEntitiesRevisionEntity.java
@@ -4,18 +4,7 @@
  */
 package org.hibernate.envers.enhanced;
 
-import java.util.HashSet;
-import java.util.Set;
-import jakarta.persistence.Column;
-import jakarta.persistence.ElementCollection;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.MappedSuperclass;
-
-import org.hibernate.annotations.Fetch;
-import org.hibernate.annotations.FetchMode;
-import org.hibernate.envers.ModifiedEntityNames;
+import jakarta.persistence.Entity;
 
 /**
  * Extension of standard {@link SequenceIdRevisionEntity} that allows tracking entity names changed in each revision.
@@ -24,57 +13,6 @@ import org.hibernate.envers.ModifiedEntityNames;
  *
  * @author Lukasz Antoniak (lukasz dot antoniak at gmail dot com)
  */
-@MappedSuperclass
-public class SequenceIdTrackingModifiedEntitiesRevisionEntity extends SequenceIdRevisionEntity {
-	@ElementCollection(fetch = FetchType.EAGER)
-	@JoinTable(name = "REVCHANGES", joinColumns = @JoinColumn(name = "REV"))
-	@Column(name = "ENTITYNAME")
-	@Fetch(FetchMode.JOIN)
-	@ModifiedEntityNames
-	private Set<String> modifiedEntityNames = new HashSet<>();
-
-	@SuppressWarnings("unused")
-	public Set<String> getModifiedEntityNames() {
-		return modifiedEntityNames;
-	}
-
-	@SuppressWarnings("unused")
-	public void setModifiedEntityNames(Set<String> modifiedEntityNames) {
-		this.modifiedEntityNames = modifiedEntityNames;
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		if ( this == o ) {
-			return true;
-		}
-		if ( !(o instanceof SequenceIdTrackingModifiedEntitiesRevisionEntity) ) {
-			return false;
-		}
-		if ( !super.equals( o ) ) {
-			return false;
-		}
-
-		final SequenceIdTrackingModifiedEntitiesRevisionEntity that = (SequenceIdTrackingModifiedEntitiesRevisionEntity) o;
-
-		if ( modifiedEntityNames == null ) {
-			return that.modifiedEntityNames == null;
-		}
-		else {
-			return modifiedEntityNames.equals( that.modifiedEntityNames );
-		}
-	}
-
-	@Override
-	public int hashCode() {
-		int result = super.hashCode();
-		result = 31 * result + (modifiedEntityNames != null ? modifiedEntityNames.hashCode() : 0);
-		return result;
-	}
-
-	@Override
-	public String toString() {
-		return "SequenceIdTrackingModifiedEntitiesRevisionEntity(" + super.toString()
-				+ ", modifiedEntityNames = " + modifiedEntityNames + ")";
-	}
+@Entity
+public final class SequenceIdTrackingModifiedEntitiesRevisionEntity extends SequenceIdTrackingModifiedEntitiesRevisionMapping {
 }

src/test/java/org/hibernate/orm/test/envers/CustomRevisionEntityTest.java
@@ -8,7 +8,7 @@ import java.util.Date;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.envers.Audited;
-import org.hibernate.envers.DefaultRevisionEntity;
+import org.hibernate.envers.RevisionMapping;
 import org.hibernate.envers.RevisionEntity;
 import org.hibernate.envers.RevisionListener;
 
@@ -124,7 +124,7 @@ public class CustomRevisionEntityTest {
 	@Entity(name = "CustomRevisionEntity")
 	@Table(name = "CUSTOM_REV_INFO")
 	@RevisionEntity(CustomRevisionEntityListener.class)
-	public static class CustomRevisionEntity extends DefaultRevisionEntity {
+	public static class CustomRevisionEntity extends RevisionMapping {
 
 		private String username;
 

src/test/java/org/hibernate/orm/test/envers/EntityTypeChangeAuditDefaultTrackingTest.java
@@ -19,7 +19,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.engine.spi.SessionFactoryImplementor;
 import org.hibernate.envers.Audited;
-import org.hibernate.envers.DefaultTrackingModifiedEntitiesRevisionEntity;
+import org.hibernate.envers.TrackingModifiedEntitiesRevisionMapping;
 import org.hibernate.envers.RevisionEntity;
 import org.hibernate.jpa.boot.spi.Bootstrap;
 
@@ -198,7 +198,7 @@ public class EntityTypeChangeAuditDefaultTrackingTest extends EntityManagerFacto
 	@Table(name = "TRACKING_REV_INFO")
 	@RevisionEntity
 	public static class CustomTrackingRevisionEntity
-		extends DefaultTrackingModifiedEntitiesRevisionEntity {
+		extends TrackingModifiedEntitiesRevisionMapping {
 
 	}
 	//end::envers-tracking-modified-entities-revchanges-example[]

src/test/java/org/hibernate/orm/test/envers/EntityTypeChangeAuditTest.java
@@ -25,7 +25,7 @@ import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.engine.spi.SessionFactoryImplementor;
 import org.hibernate.envers.AuditReaderFactory;
 import org.hibernate.envers.Audited;
-import org.hibernate.envers.DefaultRevisionEntity;
+import org.hibernate.envers.RevisionMapping;
 import org.hibernate.envers.ModifiedEntityNames;
 import org.hibernate.envers.RevisionEntity;
 import org.hibernate.jpa.boot.spi.Bootstrap;
@@ -238,7 +238,7 @@ public class EntityTypeChangeAuditTest extends EntityManagerFactoryBasedFunction
 	@Entity(name = "CustomTrackingRevisionEntity")
 	@Table(name = "TRACKING_REV_INFO")
 	@RevisionEntity
-	public static class CustomTrackingRevisionEntity extends DefaultRevisionEntity {
+	public static class CustomTrackingRevisionEntity extends RevisionMapping {
 
 		@ElementCollection
 		@JoinTable(

src/test/java/org/hibernate/orm/test/envers/integration/reventity/InheritedRevEntity.java
@@ -7,12 +7,12 @@ package org.hibernate.orm.test.envers.integration.reventity;
 import jakarta.persistence.Entity;
 
 import org.hibernate.envers.RevisionEntity;
-import org.hibernate.envers.enhanced.SequenceIdRevisionEntity;
+import org.hibernate.envers.enhanced.SequenceIdRevisionMapping;
 
 /**
  * @author Adam Warski (adam at warski dot org)
  */
 @Entity
 @RevisionEntity
-public class InheritedRevEntity extends SequenceIdRevisionEntity {
+public class InheritedRevEntity extends SequenceIdRevisionMapping {
 }